32c297b64fc9e01041f3f31f9e883154de7690a0
Syed Ali Asar <sa.asar14@gmail.com>
2025-07-12 17:13:28 -0400

    test footer

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 import Header from "../components/Header";
 import { projects } from "./projects";
 
